a679a381b57ccc31d0d3531423b9e4a936371ca8
SIDDHANT THAKUR <siddhant.76333@purc.tu.edu.np>
2022-06-08 20:54:29 +0545

    4th commit

--- Circumference.java
+++ Circumference.java
@@ -1,12 +1,18 @@
 // Write a function that takes in the radius as input and returns the circumference of a circle.
+import java.util.Scanner;
 public class Circumference {
-    public void circumference
+    public float circumference(int r){
+        float pi = 3.14F;
+        return 2*pi*r;
+    }
     public static void main(String[] args){
         Scanner num= new Scanner(System.in);
-        System.out.println("Enter two numbers");
+        System.out.println("Enter the radius:");
         int radius = num.nextInt();
         Circumference obj =new Circumference();
-        obj.circumference(num1,num2);
+        float Ans = obj.circumference(radius);
+        System.out.println(Ans + " is the obtained circumference");
         num.close();
     }
+    
 }
